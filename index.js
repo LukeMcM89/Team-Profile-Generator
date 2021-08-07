3eb7508c86c5d3524820da2c2dfa256124301a6a
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-08-07 13:59:02 -0400

    Prompt initials

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-const 
+const Employee = require('./libEmployee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+
+const questionprompt = () =>
+return inquirer.prompt([
+    {
+        type:
+        message:
+        name:
+}
+{
+        type:
+        message:
+        name:
+}
+]
