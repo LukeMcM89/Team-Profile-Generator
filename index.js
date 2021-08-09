69150fbd1d7a01ebff252415bf781ec7bb22649d
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-08-09 15:30:05 -0400

    Prompt overhaul/Adding spread for questions/more function initials

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const htmlgenerated = ('./dist/TeamRoster');
-const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
@@ -17,115 +16,75 @@ const Manager = require('./lib/manager');
 //WHEN I decide to finish building my team
 //THEN I exit the application, and the HTML is generated
 
-const managerprompt = () => {
-return inquirer.prompt([
-        {
-                type: "input",
-                message: "Please enter your Manager's name.",
-                name: "managerName"
+const team = [];
+const employeeQuestions = [
+        {message: "What is the employee's name?" name: "Name"},
+        {message: "What is the employee's email?" name: "Email"},
+        {message: "What is the employee's ID?" name: "id"}
+];
 
-        },
-        {
-                type: "input",
-                message: "Please enter your Manager's office ID.",
-                name: "managerId"
-        },
-        {
-                type: "input",
-                message: "Please enter your Manager's email address.",
-                name: "managerEmail"
-        },
-        {
-                type: "input",
-                message: "Please enter your Manager's office number.",
-                name: "managerOffice"
-        },
-        {
-                type: "list",
-                message: "Please select a team member to add to your roster.",
-                name: "addRoster",
-                choices: ['Employee', 'Engineer', 'Intern', 'Finish Roster Additions'],
-        },
-])
+const managerQuestion =
+const engineerQuestion =
+const internQuestion =
 
-.then(answers => {
-fs.writeFileSync(htmlgenerated,"");
-}
-);
 
-function addEmployee() {
-
-const engineerprompt = () => {
-return inquirer.prompt([
-        {
-                type: "input",
-                message: "Please enter your Engineer's name.",
-                name: "engineerName"
-
-        },
-        {
-                type: "input",
-                message: "Please enter your Engineer's office ID.",
-                name: "engineerId"
-        },
-        {
-                type: "input",
-                message: "Please enter your Engineer's email address.",
-                name: "engineerEmail"
-        },
-        {
-                type: "input",
-                message: "please enter your Engineer's GitHub username.",
-                name:"engineerGithub"
-        },
-        {
-                type: "list",
-                message: "Please select a team member to add to your roster.",
-                name: "addRoster",
-                choices: ['Employee', 'Engineer', 'Intern', 'Finish Roster Additions'],
-        },
-
-])
-
-function addEmployee() {
-
-const internprompt = () => {
-        return inquirer.prompt([
-        {
-                type: "input",
-                message: "Please enter your Intern's name.",
-                name: "internName"
-        
-        },
-        {
-                type: "input",
-                message: "Please enter your Intern's office ID.",
-                name: "internId"
-        },
-        {
-                type: "input",
-                message: "Please enter your Intern's email address.",
-                name: "internEmail"
-        },
-        {
-                type: "input",
-                message: "Please enter the school that your Intern attends.",
-                name:"internSchool"
-        },
-        {
-                type: "list",
-                message: "Please select a team member to add to your roster.",
-                name: "addRoster",
-                choices: ['Employee', 'Engineer', 'Intern', 'Finish Roster Additions'],
-        },
-        
-])
-
-}
+init ();
 
 function init() {
-        managerprompt, engineerprompt, internprompt ();
-    };
+        console.log("Welcome! Build your Software Engineer Team Roster.");
+        mainMenu();
+}
+
+function mainMenu(){
+        inquieer.prompt(){
+                message:
+                name:
+                type:
+                choices: 
+        }
+}
 
 
-    init(); 
+
+                function addEmployee() {
+
+                        const internprompt = () => {
+                                return inquirer.prompt([
+                                        {
+                                                type: "input",
+                                                message: "Please enter your Intern's name.",
+                                                name: "internName"
+
+                                        },
+                                        {
+                                                type: "input",
+                                                message: "Please enter your Intern's office ID.",
+                                                name: "internId"
+                                        },
+                                        {
+                                                type: "input",
+                                                message: "Please enter your Intern's email address.",
+                                                name: "internEmail"
+                                        },
+                                        {
+                                                type: "input",
+                                                message: "Please enter the school that your Intern attends.",
+                                                name: "internSchool"
+                                        },
+                                        {
+                                                type: "list",
+                                                message: "Please select a team member to add to your roster.",
+                                                name: "addRoster",
+                                                choices: ['Employee', 'Engineer', 'Intern', 'Finish Roster Additions'],
+                                        },
+
+                                ])
+
+                        }
+
+                        function init() {
+                                managerprompt, engineerprompt, internprompt();
+                        };
+
+
+                        init();
